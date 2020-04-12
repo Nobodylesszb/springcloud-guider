60c7c269bcccd4cded4c3d31e73522dd34b5b7b3
Nobodylesszb <nobodylesszb@163.com>
2020-04-12 20:08:08 +0800

    增加spring-admin模块学习

---
+++ admin-client/src/main/java/com/bo/adminclient/service/UserService.java
@@ -0,0 +1,21 @@
+package com.bo.adminclient.service;
+
+
+import com.bo.adminclient.domain.User;
+
+import java.util.List;
+
+
+public interface UserService {
+    void create(User user);
+
+    User getUser(Long id);
+
+    void update(User user);
+
+    void delete(Long id);
+
+    User getByUsername(String username);
+
+    List<User> getUserByIds(List<Long> ids);
+}
