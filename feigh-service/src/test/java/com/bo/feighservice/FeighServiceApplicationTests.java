c4eba476ea2972377152d6925a45b8588a43a8ef
Nobodylesszb <nobodylesszb@163.com>
2020-04-12 22:15:16 +0800

    增加feign模块

---
+++ feigh-service/src/test/java/com/bo/feighservice/FeighServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bo.feighservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeighServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
