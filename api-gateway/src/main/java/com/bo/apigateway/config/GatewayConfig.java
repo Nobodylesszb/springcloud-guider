30dbf53281b1c507e1bf89997dafb193afaba16d
Nobodylesszb <nobodylesszb@163.com>
2020-04-13 11:01:12 +0800

    增加熔断限流模块

---
+++ api-gateway/src/main/java/com/bo/apigateway/config/GatewayConfig.java
@@ -0,0 +1,21 @@
+package com.bo.apigateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @Auther: bo
+ * @Date: 2020/4/12 18:00
+ * @Version:
+ * @Description:
+ */
+public class GatewayConfig {
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("path_route2", r -> r.path("/user/getByUsername")
+                        .uri("http://localhost:8201/user/getByUsername"))
+                .build();
+    }
+}
