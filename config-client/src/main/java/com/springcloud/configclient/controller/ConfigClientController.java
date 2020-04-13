74acf1066b678f5ce383c45c823f37b26259ecaa
Nobodylesszb <nobodylesszb@163.com>
2020-04-13 11:52:24 +0800

    增加配置中心模块

---
+++ config-client/src/main/java/com/springcloud/configclient/controller/ConfigClientController.java
@@ -0,0 +1,25 @@
+package com.springcloud.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Auther: bo
+ * @Date: 2020/4/13 11:48
+ * @Version:
+ * @Description:
+ */
+
+@RestController
+@RefreshScope
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
